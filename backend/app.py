bb56ea384b0f70df55273dd42c1055b1c5dbed9f
Mohd Zahid <md.zahid786@gmail.com>
2025-09-13 16:28:53 +0530

    changed in the file path

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 load_dotenv()
 #uri = os.getenv('MONGO_URL')
 port = int(os.getenv('PORT', 5000))
-
+storage_path = os.getenv("BACKEND_STORAGE_PATH", "/data")
+os.makedirs(storage_path, exist_ok=True)
+data_file = os.path.join(storage_path, "test.json")
 @app.route("/api/health")
 def health():
     return jsonify({"message":"Healthy..."})
@@ -17,7 +19,7 @@ def getData():
     if not data:
         return jsonify({"error": "No JSON data received"}), 400
     try:
-        with open("test.json", "r+") as file:
+        with open(data_file, "r+") as file:
             content = file.read().strip()
             if content:
                 extended_data = json.loads(content)
@@ -29,7 +31,7 @@ def getData():
     
     # Save it to a file
     extended_data.append(data)
-    with open("test.json", "w") as file:
+    with open(data_file, "w") as file:
         json.dump(extended_data, file)
 
     return jsonify({"message":"success"})
